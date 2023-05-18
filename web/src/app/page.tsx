64b721420d986f76c7cd8671c4a92310291377dc
ThalesZZ <thazars@hotmail.com>
2023-05-18 02:17:43 -0300

    displaying user profile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,20 @@
 import Copyright from '@/components/Copyright'
 import EmptyMemories from '@/components/EmptyMemories'
 import Hero from '@/components/Hero'
+import Profile from '@/components/Profile'
 import SignIn from '@/components/SignIn'
+import { cookies } from 'next/headers'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] px-28 py-16">
         <Blur />
         <Stripes />
 
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
